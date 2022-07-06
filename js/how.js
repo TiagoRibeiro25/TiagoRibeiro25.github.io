b59376c144ed60ff51aba294911e5b14d0048da4
TiagoRibeiro25 <40210462@esmad.ipp.pt>
2022-07-06 23:55:19 +0100

    added comments to how.js

--- js/how.js
+++ js/how.js
@@ -1,8 +1,12 @@
 // Add Event Listeners to the discord button
 document.querySelector("#discordNameBtn").addEventListener("click", () => {
+  // Copy the discord name/id to the clipboard
   navigator.clipboard.writeText("-|GOLD|-#5063");
+
+  // Show the discord name/id copied message
   document.querySelector("#discordMsg").style.display = "block";
 
+  // Hide the discord name/id copied message after 5 seconds
   setTimeout(() => {
     document.querySelector("#discordMsg").style.display = "none";
   }, 5000);
